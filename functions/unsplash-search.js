33688c826492c925ac14cad687060df6f4d1b024
Cynthia Teeters <glean@cynthiateeters.com>
2021-04-09 17:46:39 -0400

    Init

---
+++ functions/unsplash-search.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+  const { query } = JSON.parse(event.body);
+
+  const response = await fetch(`https://api.unsplash.com/search/photos?query=${query}&order_by=popular`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Client-ID ${process.env.UNSPLASH_API_TOKEN}`,
+    },
+  })
+    .then((response) => response.json())
+    .catch((error) => console.log('error', error));
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(response),
+  };
+};
